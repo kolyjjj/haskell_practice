e0fe34be73f42c8c4a33ad2da080bff225434565
kolyjjj <me.koly.li@gmail.com>
2015-12-07 17:08:32 +0800

    reverse the input line

--- helloworld.hs
+++ helloworld.hs
@@ -1,5 +1,25 @@
 -- main = putStrLn "hello, world"
+{-
+printHello :: IO ()
+printHello = putStrLn "hello"
+-}
+
+{-
 main = do
+    printHello
     putStrLn "Hello, what's your name?"
     name <- getLine
     putStrLn ("Welcome on board, " ++ name)
+-}
+
+reverseWords :: String -> String
+reverseWords [] = []
+reverseWords (x:xs) = (reverseWords xs) ++ [x]
+
+main = do
+    line <- getLine
+    if null line
+    then return ()
+    else do
+        putStrLn $ reverseWords line
+        main
