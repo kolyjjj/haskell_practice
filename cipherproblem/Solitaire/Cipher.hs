5ccc34e40f0ba168eaf9f899a111c7e2f0b0d016
kolyjjj <me.koly.li@gmail.com>
2015-12-08 13:21:35 +0800

    add method to append 'X

--- cipherproblem/Solitaire/Cipher.hs
+++ cipherproblem/Solitaire/Cipher.hs
@@ -7,12 +7,23 @@ where
 
 import Data.Char as DChar
 
+append_char :: Char
+append_char = 'X'
+
 discard :: String -> String
 discard input = [x | x <- input, (x < 'z' && x > 'a') || (x <= 'Z' && x >='A')]
 
 toUpperCase :: String -> String
 toUpperCase [] = []
 toUpperCase (x:xs) = [DChar.toUpper x] ++ toUpperCase xs
+
+appendX :: String -> String
+appendX input
+    | remainder == 0 = input
+    | otherwise = input ++ replicate (5-remainder) 'X'
+    where
+    len = length input
+    remainder = len `mod` 5
 
 encrypt :: String -> String
 encrypt input = discard(input)
