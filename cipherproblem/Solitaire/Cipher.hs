1bbd2cd384e8d25f5d47cfba39395d32eb90f754
kolyjjj <me.koly.li@gmail.com>
2015-12-08 10:56:04 +0800

    add more functions for cipher

--- cipherproblem/Solitaire/Cipher.hs
+++ cipherproblem/Solitaire/Cipher.hs
@@ -5,8 +5,14 @@ decrypt
 )
 where
 
+import Data.Char as DChar
+
 discard :: String -> String
 discard input = [x | x <- input, (x < 'z' && x > 'a') || (x <= 'Z' && x >='A')]
+
+toUpperCase :: String -> String
+toUpperCase [] = []
+toUpperCase (x:xs) = [DChar.toUpper x] ++ toUpperCase xs
 
 encrypt :: String -> String
 encrypt input = discard(input)
