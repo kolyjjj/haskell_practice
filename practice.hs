c369ee8bb9717431dfa24cdaf651cc5efe58b50b
kolyjjj <me.koly.li@gmail.com>
2015-12-09 16:34:55 +0800

    implement type class

--- practice.hs
+++ practice.hs
@@ -258,3 +258,21 @@ koly = Person {
     phoneNumber = "123124123123",
     flavor = "light"
 }
+
+-- implement type class
+data TrafficLight = Red | Yellow | Green
+instance Eq TrafficLight where
+    Red == Red = True
+    Yellow == Yellow = True
+    Green == Green = True
+    _ == _ = False
+instance Show TrafficLight where
+    show Red = "Red Light"
+    show Yellow = "Yellow Light"
+    show Green = "Green Light"
+
+passingRoad :: TrafficLight -> String
+passingRoad light
+    | light == Red = "Red light, you cannot pass"
+    | light == Yellow = "Yellow light, you cannot pass"
+    | light == Green = "Green light, you can pass"
