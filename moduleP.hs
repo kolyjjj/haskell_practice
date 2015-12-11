0dee43a9b9ace8b407d1d0fbc1da7dc24fd8571e
kolyjjj <me.koly.li@gmail.com>
2015-12-11 10:10:08 +0800

    implement normal phone book

--- moduleP.hs
+++ moduleP.hs
@@ -1,5 +1,6 @@
 import Data.List
 import Data.Char
+import qualified Data.Map as D
 
 wordCount = words "hey, these are the words in this sentence"
 wordsGroup = group ["boom", "bip", "boom", "bip"]
@@ -14,3 +15,23 @@ caesarDecode :: Int -> String -> String
 caesarDecode num = caesarEncode (negate num)
 
 originalCodes = caesarDecode 5 $ caesarEncode 5 "hello, Caesar"
+
+phoneBook :: D.Map String String
+phoneBook = D.fromList $
+    [
+        ("betty", "555-2923"),
+        ("putin", "255-2923"),
+        ("bejing", "355-2923"),
+        ("bella", "455-2923")
+    ]
+
+normalPhoneBook :: [(String, String)]
+normalPhoneBook = [
+       ("betty", "555-2923"),
+       ("putin", "255-2923"),
+       ("bejing", "355-2923"),
+       ("bella", "455-2923")
+    ]
+
+findByKey :: String -> [(String, String)] -> String
+findByKey s = snd . head . filter (\(x, y) -> x == s)
